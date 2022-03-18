63c28aed38e915ecf0fa1c055ab2c41c54036c63
AlphaKuma <raphaeltruong27@outlook.com>
2022-03-18 17:24:58 +0100

    add css-gradient component

---
+++ src/css-gradient/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import ColorPicker from './color';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
